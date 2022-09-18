37440529779955be94344ffd1fd2310757e37e34
charole <rjs985939@gmail.com>
2022-09-18 12:08:09 +0900

    feat: 게시판 CRUD 추가

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from .views import *
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = 'main'
 
 urlpatterns = [
     path('', index),
-    path('blog/', blog)
+    path('blog/', blog),
+    path('blog/<int:pk>', posting),
+    path('blog/new_post', new_post),
+    path('blog/<int:pk>/remove', remove_post),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
