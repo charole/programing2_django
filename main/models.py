e2a14515728c05788cce91c5958c5eb0c4cb273b
seyeon <yscat618@gmail.com>
2022-11-21 11:45:42 +0900

    add exam_type field

--- main/models.py
+++ main/models.py
@@ -24,10 +24,16 @@ class Account(models.Model):
 
 
 class Example(models.Model):
+    EXAM_TYPES = (
+        ('Multiple', 'Multiple'),
+        ('Simple', 'Simple')
+    )
+
     id = models.AutoField(db_column='id', primary_key=True)
     title = models.CharField(max_length=100)
     content = models.TextField(default='')
     exam_question = models.TextField(default='', null=True, blank=True)
+    exam_type = models.CharField(choices=EXAM_TYPES, max_length=10)
     answer = models.TextField(default='')
     example = models.TextField(default='', null=True, blank=True)
     hint = models.TextField(default='', null=True, blank=True)
